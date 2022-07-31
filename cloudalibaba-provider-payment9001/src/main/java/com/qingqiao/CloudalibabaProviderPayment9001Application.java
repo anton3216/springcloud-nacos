e55effa5c016aa3e753a19547f69808b82a2b352
Dahuang <anton3216@163.com>
2022-08-01 01:07:35 +0800

    springcloud-nacos 备课 2022年8月1日01:07:26

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/qingqiao/CloudalibabaProviderPayment9001Application.java
@@ -0,0 +1,14 @@
+package com.qingqiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class CloudalibabaProviderPayment9001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudalibabaProviderPayment9001Application.class, args);
+    }
+
+}
